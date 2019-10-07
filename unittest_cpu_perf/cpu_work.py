183515e71715799aab2154f6c27bd50039a1f20f
jumbobumbo <MasterCh0der!*>
2019-10-07 20:15:31 +0100

    added further cpu tests, threading

--- unittest_cpu_perf/cpu_work.py
+++ unittest_cpu_perf/cpu_work.py
@@ -1,12 +1,13 @@
 from random import randint
+import time
+from pathlib import Path
 
 
 class WorkCPU:
 
     @staticmethod
     def two_lists():
-        for x, y in zip([randint(0, 5) for i in range(0, 20000)], [randint(5, 9) for i in range(0, 20000)]):
-            if x - y == 0:
-                print("equal")
-            else:
-                print("not equal")
+        with open(Path("output", str(str(time.time()) + "_equal_log.txt")), "w") as f:
+            for x, y in zip([randint(0, 6) for i in range(0, 20000)], [randint(4, 9) for i in range(0, 20000)]):
+                if x - y == 0:
+                    f.write(f"{x}\n")
--- unittest_cpu_perf/test_psutil.py
+++ unittest_cpu_perf/test_psutil.py
@@ -1,16 +1,49 @@
 import unittest
 from parameterized import parameterized
 import psutil
+import threading
 from cpu_work import WorkCPU as WC
+
+
+class Asserter:
+
+    @staticmethod
+    def assert_less(self, current, maximum):
+        """
+        asserts current is below maximum
+        self: unittestObject
+        current: int/float
+        maximum: int/float
+        """
+        self.assertLess(current, maximum, f"{current}% cpu usage is higher than max val: {maximum}")
 
 
 class TestCpu(unittest.TestCase):
     """ Various tests regarding the host systems CPU """
+
+    def test_cpu_before_compute(self):
+        """ should fail if the machine is idling at 5 + % """
+        Asserter.assert_less(self, psutil.cpu_percent(), 20)
+
     @parameterized.expand([
         (50, ),
-        (20, ),
+        (25, ),
     ])
     def test_cpu_load_after_compute(self, max_util):
+        """ tests cpu usage after heavy lifting """
         WC.two_lists()
-        cpu_usage = psutil.cpu_percent()
-        self.assertLess(cpu_usage, max_util, f"{cpu_usage}% cpu usage is too high")
+        Asserter.assert_less(self, psutil.cpu_percent(), max_util)
+
+    @parameterized.expand([
+        (60,),
+        (40,),
+        (30,),
+    ])
+    def test_cpu_during_compute(self, max_util):
+        """ verify cpu usage isn't above stated values during compute """
+        w = threading.Thread(name="lists_gen", target=WC.two_lists())
+        t = threading.Thread(name="cpu_assert", target=Asserter.assert_less(self, psutil.cpu_percent(), max_util))
+        w.start()
+        t.start()
+        w.join()
+        t.join()
